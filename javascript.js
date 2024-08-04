2dc9c8460f59dc04ad34380f0eab2dfd31fb2b47
Rohini.R <rohinirachelr@gmail.com>
2024-08-04 23:13:43 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,66 @@
+function wrong1(){
+    document.querySelector(".a").style.backgroundColor="red";
+ }
+ function wrong2(){
+     document.querySelector(".b").style.backgroundColor="red";
+  }
+ function wrong3(){
+     document.querySelector(".c").style.backgroundColor="red";
+  }
+ function correct(){
+    document.querySelector(".d").style.backgroundColor="green";
+    document.querySelector("#e").style.backgroundColor="green";
+ }
+ 
+ function wrong11(){
+    document.querySelector(".a1").style.backgroundColor="red";
+ }
+ function wrong12(){
+     document.querySelector(".b1").style.backgroundColor="red";
+  }
+ function wrong13(){
+     document.querySelector(".c1").style.backgroundColor="red";
+  }
+ function correct1(){
+    document.querySelector(".d1").style.backgroundColor="green";
+    document.querySelector("#e1").style.backgroundColor="green";
+ }
+ function wrong21(){
+    document.querySelector(".a2").style.backgroundColor="red";
+ }
+ function wrong22(){
+     document.querySelector(".b2").style.backgroundColor="red";
+  }
+ function wrong23(){
+     document.querySelector(".c2").style.backgroundColor="red";
+  }
+ function correct2(){
+    document.querySelector(".d2").style.backgroundColor="green";
+    document.querySelector("#e2").style.backgroundColor="green";
+ }
+ function wrong31(){
+    document.querySelector(".a3").style.backgroundColor="red";
+ }
+ function wrong32(){
+     document.querySelector(".b3").style.backgroundColor="red";
+  }
+ function wrong33(){
+     document.querySelector(".c3").style.backgroundColor="red";
+  }
+ function correct3(){
+    document.querySelector(".d3").style.backgroundColor="green";
+    document.querySelector("#e3").style.backgroundColor="green";
+ }
+ function wrong41(){
+    document.querySelector(".a4").style.backgroundColor="red";
+ }
+ function wrong42(){
+     document.querySelector(".b4").style.backgroundColor="red";
+  }
+ function wrong43(){
+     document.querySelector(".c4").style.backgroundColor="red";
+  }
+ function correct4(){
+    document.querySelector(".d4").style.backgroundColor="green";
+    document.querySelector("#e4").style.backgroundColor="green";
+ } 
